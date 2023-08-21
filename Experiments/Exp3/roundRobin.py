46ffc84c3faed74544efa21fecdc657101ec4498
Aatmaj <Aatmaj.mhatre@gmail.com>
2023-08-21 21:02:39 +0530

    update roundRobin.py

--- Experiments/Exp3/roundRobin.py
+++ Experiments/Exp3/roundRobin.py
@@ -1,25 +1,32 @@
+'''
 k=int(input("Please enter number of processes "))
 
 arrivalTime=[int(input("Please enter arrival time for process "+str(i)+" ")) for i in range(k)]
 
 reqTime=[int(input("Please enter estimated time for process "+str(i)+" ")) for i in range(k)]
 
-processes=range(k)
+
 timeQuantaDecided =int(input("Please enter the time quanta "))
 
 '''
 #Test case 
-
+k=4
 arrivalTime=[0,1,3,2]
-reqTime=[8,4,2,3]
 processes=[0,1,2,3]
 timeQuantaDecided =2
 
-'''
+k=5
+arrivalTime=[0,2,4,6,7]
+reqTime=[2,3,4,5,6]
+timeQuantaDecided =2
 
+processes=range(k)
 timeQuanta = timeQuantaDecided
+reqTimeCopy=[i for i in reqTime]
 queue=[]
 answer=[]
+waiting=[0 for i in range(k)]
+
 for i in range(100):
     
     for process in processes: #check new processes comming
@@ -38,13 +45,18 @@ for i in range(100):
     currentProcess=queue[0]
     print(i,"\t", currentProcess,"\t\t\t",queue)
     
-   
+    for waitingProcess in processes:
+        # if process arrived and waiting and present in queue(not terminated)
+        if(waitingProcess!=currentProcess and arrivalTime[waitingProcess]<=i and waitingProcess in queue):
+            waiting[waitingProcess]+=1
     answer.append(currentProcess)
     reqTime[currentProcess]-=1
     timeQuanta-=1 
-     
+    
+print("Total time spent waiting for processes is given by", waiting) 
+print("Total turnaround time for processes is given by", [int(waiting[i])+int(reqTimeCopy[i]) for i in processes])      
 print("\n")    
-print("\n_____________________________________________________________________________________________________________________________________________________")
+print("\n_________________________________________________________________________________________________________________________________________________________________")
 
 temp=-1
 for i in range(0,len(answer)):
@@ -61,6 +73,3 @@ for i in range(0,len(answer)):
 print("|\n-----------------------------------------------------------------------------------------------------------------------------------------------------------------")
 for i in range(0,len(answer)+1):
      print(i,end="\t")
-
-        
-        
