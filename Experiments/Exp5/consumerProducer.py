4096cf7481facb6f8992c27372e673a50574ff65
Aatmaj <Aatmaj.mhatre@gmail.com>
2023-10-09 17:55:58 +0530

    Create consumerProducer.py

---
+++ Experiments/Exp5/consumerProducer.py
@@ -0,0 +1,56 @@
+# function to simulate consumer producer
+import time
+max_len = 10
+def produce(n,mutex,pointer):
+    for i in range(10):
+            if(pointer.value<max_len):
+                
+                if   (mutex.value==0):
+                    mutex.value = 1 #flop
+                    pointer.value+=1
+                    n[pointer.value]=1
+                    print("I am producing at",pointer.value )
+                    for i in n:
+                          print(i,end=",")
+                    print()
+                mutex.value = 0 #flop
+
+                
+            time.sleep(0.2)
+
+def consume(n,mutex,pointer):
+    for i in range(10):
+        if(pointer.value>=0):
+            if   (mutex.value==0):
+                mutex.value = 1 #flip
+                n[pointer.value]=0
+                print("I am consuming at" ,pointer.value)
+                for i in n:
+                          print(i,end=",")
+                print()
+                pointer.value-=1
+
+            mutex.value = 0 #flip
+        time.sleep(0.5)
+
+                
+if __name__ == '__main__': # dont remove this line code wont run in windows.
+    import multiprocessing
+
+    # Define a shared variable
+    n =  multiprocessing.Array('i', max_len)
+    mutex = multiprocessing.Value('i',0)
+    pointer = multiprocessing.Value('i',-1)
+
+   
+
+    # Create two processes that run the increment function
+    process1 = multiprocessing.Process(target=produce,args=(n,mutex,pointer,))
+    process2 = multiprocessing.Process(target=consume,args=(n,mutex,pointer,))
+
+    # Start both processes
+    process1.start()
+    process2.start()
+
+    
+    
